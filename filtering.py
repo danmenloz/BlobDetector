a079828118db40e4c2052ad96b7fb3b2c416ec06
Daniel Mendez <dmendez@ncsu.edu>
2019-11-24 17:35:17 -0500

    Added step 5.

--- filtering.py
+++ filtering.py
@@ -8,11 +8,12 @@ import pyfftw # FFT library
 import time
 
 
-img_file = './Images/lena.png'
+img_file = './Images/sunflowers.jpg'
 img = cv.imread(img_file)
 nrows = img.shape[0]
 ncols = img.shape[1]
 nchls = img.shape[2]
+print("img: " + str(img.shape))
 
 # Step 1
 # padded sizes P Q
@@ -25,7 +26,7 @@ print("P,Q: " + str(P) + ", " + str(Q))
 img_padded = dip.padding(img, dip.Pad.REFLECT_ACROSS_EDGE, mn=(nrows+1,ncols+1) ) # +1 because of the shared border with kernel
 # crop padded image
 img_cropped = img_padded[nrows:,ncols:]
-print(img_cropped.shape)
+print("img_cropped: " + str(img_cropped.shape))
 
 # Step 3
 # multiply by (-1)^(x+y)
@@ -38,6 +39,25 @@ for ch in range(nchls):
 # Step 4
 # Compute DFT
 Fimg = dip.DFT2(img_shifted) # Fourier transform
+
+# Step 5
+# real-symmetric transfer funtion H(u,v) of size PxQ centerd at (P/2,Q,2)
+sigma = 2 # try different values
+ksize = max(P, Q)
+# pdb.set_trace()
+G = cv.getGaussianKernel(ksize,sigma)
+W = np.multiply(G,np.transpose(G))
+# pdb.set_trace()
+if Q>P:
+    wa = int((Q-P)/2)
+    W = W[wa:wa+P,:]
+elif Q<P:
+    wb = int((P-Q)/2)
+    W = W[:,wb:wb+Q]
+print("G Kernel: " + str(W.shape))
+
+
+
 
 
 
