f30b9d2e52137d3dcaf9338b74bdb5799c4267e4
Daniel Mendez <dmendez@ncsu.edu>
2019-11-24 20:10:17 -0500

    Refactored mag and phase response test case  with freqz

--- test.py
+++ test.py
@@ -11,56 +11,50 @@ import time
 
 
 
-# print("\MAGNITUDE AND PHASE PLOTS\n")
+print("\nMAGNITUDE AND PHASE PLOTS\n")
+img_file = './Images/lena.png'
+img = cv.imread(img_file,cv.IMREAD_GRAYSCALE)
+# Call diptool function
+[hmag, hphase] = dip.freqz(img, normalized=True, centered=True)
+# plot responses
+cv.imshow('Original Image',img)
+cv.imshow('Magnitude Spectrum',hmag)
+cv.imshow('Phase Angle',hphase)
+cv.waitKey()
+cv.destroyAllWindows()
+
+
+# print("\n FORWARD AND INVERSE DFT DANIEL \n")
+# start_time = time.time()
 # img_file = './Images/lena.png'
 # img = cv.imread(img_file,cv.IMREAD_GRAYSCALE)
-# img_scld = dip.scaleImageChannel(img,1,np.float32) # scale image to range [0,1]
-# F = dip.DFT2(img_scld) # compute DFT2
-# F_shift = np.fft.fftshift(F)
-# magnitude_spectrum = np.log(1+np.abs(F_shift)) # 1+np.abs(F_shift) is the log tranformation, just for visualization
-# magnitude_spectrum = dip.scaleImageChannel(magnitude_spectrum,255,np.uint8)
-# phase_angle = np.angle(F_shift)
-# phase_angle = dip.scaleImageChannel(phase_angle,255,np.uint8)
+# Fimg = dip.DFT2_PYFFTW(img) # Fourier transform
+# fimg_c = dip.IDFT2__PYFFTW(Fimg) # Inverse Fourier transform
+# fimg = fimg_c.astype(np.uint8) # Real part only and convert to uint
+# error = (img-fimg).astype(np.uint8) # compute error
+# print("    time(s): " + str(time.time()-start_time))
+# print('Max error: ' + str(np.max(error)))
 # cv.imshow('Original Image',img)
-# cv.imshow('Magnitude Spectrum',magnitude_spectrum)
-# cv.imshow('Phase Angle',phase_angle)
+# cv.imshow('IDFT2 Result',fimg)
+# cv.imshow('IDFT2 Check',error)
 # cv.waitKey()
 # cv.destroyAllWindows()
-
-
-
-
-print("\n FORWARD AND INVERSE DFT DANIEL \n")
-start_time = time.time()
-img_file = './Images/lena.png'
-img = cv.imread(img_file,cv.IMREAD_GRAYSCALE)
-Fimg = dip.DFT2_PYFFTW(img) # Fourier transform
-fimg_c = dip.IDFT2__PYFFTW(Fimg) # Inverse Fourier transform
-fimg = fimg_c.astype(np.uint8) # Real part only and convert to uint
-error = (img-fimg).astype(np.uint8) # compute error
-print("    time(s): " + str(time.time()-start_time))
-print('Max error: ' + str(np.max(error)))
-cv.imshow('Original Image',img)
-cv.imshow('IDFT2 Result',fimg)
-cv.imshow('IDFT2 Check',error)
-cv.waitKey()
-cv.destroyAllWindows()
-
-print("\n FORWARD AND INVERSE DFT ALIETH \n")
-start_time = time.time()
-img_file = './Images/lena.png'
-img = cv.imread(img_file,cv.IMREAD_GRAYSCALE)
-Fimg = dip.DFT2(img) # Fourier transform
-fimg_c = dip.IDFT2(Fimg) # Inverse Fourier transform
-fimg = fimg_c.astype(np.uint8) # Real part only and convert to uint
-error = (img-fimg).astype(np.uint8) # compute error
-print("    time(s): " + str(time.time()-start_time))
-print('Max error: ' + str(np.max(error)))
-cv.imshow('Original Image',img)
-cv.imshow('IDFT2 Result',fimg)
-cv.imshow('IDFT2 Check',error)
-cv.waitKey()
-cv.destroyAllWindows()
+#
+# print("\n FORWARD AND INVERSE DFT ALIETH \n")
+# start_time = time.time()
+# img_file = './Images/lena.png'
+# img = cv.imread(img_file,cv.IMREAD_GRAYSCALE)
+# Fimg = dip.DFT2(img) # Fourier transform
+# fimg_c = dip.IDFT2(Fimg) # Inverse Fourier transform
+# fimg = fimg_c.astype(np.uint8) # Real part only and convert to uint
+# error = (img-fimg).astype(np.uint8) # compute error
+# print("    time(s): " + str(time.time()-start_time))
+# print('Max error: ' + str(np.max(error)))
+# cv.imshow('Original Image',img)
+# cv.imshow('IDFT2 Result',fimg)
+# cv.imshow('IDFT2 Check',error)
+# cv.waitKey()
+# cv.destroyAllWindows()
 
 
 
