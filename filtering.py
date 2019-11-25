07af7f8b39e18121b97ac4c3e218ed8e1ab92c5c
Daniel Mendez <dmendez@ncsu.edu>
2019-11-24 20:11:17 -0500

    Debbuging filter for RGB.

--- filtering.py
+++ filtering.py
@@ -30,15 +30,18 @@ print("img_cropped: " + str(img_cropped.shape))
 
 # Step 3
 # multiply by (-1)^(x+y)
-img_shifted = np.empty(img.shape, np.int8)
+img_shifted = np.empty((P,Q,3), np.int8)
 for ch in range(nchls):
-    for y in range(nrows):
-        for x in range(ncols):
+    for y in range(P):
+        for x in range(Q):
             img_shifted[y][x][ch] = img_cropped[y][x][ch] * (-1)**(x+y)
 
 # Step 4
 # Compute DFT
-Fimg = dip.DFT2(img_shifted) # Fourier transform
+Fimg = np.empty((P,Q,3), dtype=complex)
+for ch in range(nchls):
+    Fimg[:,:,ch] = dip.DFT2(img_shifted[:,:,ch]) # Fourier transform
+    print("Fimg min,max: " + str(np.min(Fimg[:,:,ch])) + ", " + str(np.max(Fimg[:,:,ch])))
 
 # Step 5
 # real-symmetric transfer funtion H(u,v) of size PxQ centerd at (P/2,Q,2)
@@ -46,22 +49,46 @@ sigma = 2 # try different values
 ksize = max(P, Q)
 # pdb.set_trace()
 G = cv.getGaussianKernel(ksize,sigma)
-W = np.multiply(G,np.transpose(G))
+H = np.multiply(G,np.transpose(G))
 # pdb.set_trace()
 if Q>P:
-    wa = int((Q-P)/2)
-    W = W[wa:wa+P,:]
+    ha = int((Q-P)/2)
+    H = H[ha:ha+P,:]
 elif Q<P:
-    wb = int((P-Q)/2)
-    W = W[:,wb:wb+Q]
-print("G Kernel: " + str(W.shape))
+    hb = int((P-Q)/2)
+    H = H[:,hb:hb+Q]
+    H = np.asarray(H, dtype=np.double)
+print("H Kernel: " + str(H.shape))
+# pdb.set_trace()
+print("H min,max: " + str(np.min(H)) + ", " + str(np.max(H)) )
 
+# Step 6
+# H(u,v)*F(u,v)
+Gimg = np.empty((P,Q,3), dtype=complex)
+for ch in range(nchls):
+    Gimg[:,:,ch] = np.multiply(Fimg[:,:,ch], H)
+    print("Gimg min,max: " + str(np.min(Gimg[:,:,ch])) + ", " + str(np.max(Gimg[:,:,ch])))
+print("Gimg: " + str(Gimg.shape))
 
+# Step 7
+# Filtered image through iDFT
+gimg = np.empty((P,Q,3), np.double)
+g_padded = np.empty((P,Q,3), np.int8)
+for ch in range(nchls):
+    gimg[:,:,ch] = (dip.IDFT2(Gimg[:,:,ch])).real
+    print("gimg min,max: " + str(np.min(gimg[:,:,ch])) + ", " + str(np.max(gimg[:,:,ch])))
 
+for ch in range(nchls):
+    for y in range(P):
+        for x in range(Q):
+            g_padded[y,x,ch] = (gimg[y,x,ch] * (-1)**(x+y)).astype(np.int8)
+    print("g_padded min,max: " + str(np.min(g_padded[:,:,ch])) + ", " + str(np.max(g_padded[:,:,ch])))
+print("gimg: " + str(g_padded.shape))
 
 
 
 cv.imshow('Padding',img_cropped)
-cv.imshow('Shifted', dip.scaleImage(img, modo = 'custom', K = 255))
+# cv.imshow('Shifted', dip.scaleImage(img_shifted, modo = 'custom', K = 255))
+cv.imshow('Gpadded',dip.scaleImage(g_padded, modo = 'custom', K = 255))
 cv.waitKey()
 cv.destroyAllWindows()
